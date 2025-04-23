56ef47b767df44c544b68523a6ef6c4daa7eca79
Lilp Su <willy67k001@gmail.com>
2025-04-24 00:02:44 +0800

    fix: style config

--- quasar.config.js
+++ quasar.config.js
@@ -15,7 +15,7 @@ export default defineConfig((ctx) => {
     boot: ["i18n", "axios"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
-    css: ["app.scss"],
+    css: ["app.scss", "preflight.scss", "variables.scss"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
